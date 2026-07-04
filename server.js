39d42143e87229dfc4219a72601e78a88b063936
edwards guillen <edwardsguillen@gmail.com>
2026-07-03 20:30:39 -0600

    Restructurado: backend en raiz del proyecto

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(__dirname));
 
 app.post('/registro', async (req, res) => {
     try {
